f54d4f47993064f3f0aa94f2b63f6241e91433e0
Snah-s <camiloernestosoto@gmail.com>
2025-11-07 01:32:27 -0500

    ✨ Column value recongnition

--- data_and_modeling/eda.py
+++ data_and_modeling/eda.py
@@ -5,12 +5,8 @@ import seaborn as sns
 from colorama import Fore, Style
 # %%
 df = pd.read_parquet('data/events_all_wsl2021.parquet')
-df.head()
 # %%
-df.shape
-# %%
-df.describe()
-# %%
+# Mostramos información básica de cada columna
 for col in df.columns:
   s = df[col]
   dtype = s.dtype
@@ -27,3 +23,22 @@ for col in df.columns:
     f'{Fore.GREEN}Unique values:{Style.RESET_ALL} {unique_vals}, '
     f'{Fore.GREEN}Missing values:{Style.RESET_ALL} {missing}'
   )
+# %%
+# Vemos todos los valores unicos de cada columna si y solo si el numero de valores unicos es menor a 35
+for col in df.columns:
+  s = df[col]
+  try:
+    unique_vals = s.nunique()
+  except TypeError:
+    unique_vals = s.astype(str).nunique()
+  
+  if unique_vals < 35:
+    print(f'{Fore.GREEN}Column:{Style.RESET_ALL} {col}, {Fore.GREEN}Unique values:{Style.RESET_ALL} {unique_vals}')
+    try:
+      print(s.unique())
+    except TypeError:
+      print(s.astype(str).unique())
+    print('\n')
+  else:
+    print(f'{Fore.GREEN}Column:{Style.RESET_ALL} {col}, {Fore.GREEN}Unique values:{Style.RESET_ALL} {unique_vals}')
+    print('Lee el titulo')
